5ce989425840ebdc663a011c03d49a1e2a510269
kangdongil <67248006+kangdongil@users.noreply.github.com>
2020-08-19 23:32:41 +0900

    Updates

---
+++ precache-manifest.623cbc53b1febb365f907079cb1cb033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcd478b430480fbfd8e2400c20c44a8",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "1b6c6e189aa31b47600f",
+    "url": "/react_movie_app/static/css/main.7035f4b0.chunk.css"
+  },
+  {
+    "revision": "e18b1ec9f8a11dfc82ae",
+    "url": "/react_movie_app/static/js/2.75c175c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.75c175c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6c6e189aa31b47600f",
+    "url": "/react_movie_app/static/js/main.79291880.chunk.js"
+  },
+  {
+    "revision": "b2eab6cde90deae0fa53",
+    "url": "/react_movie_app/static/js/runtime-main.e749ff8d.js"
+  }
+]);
